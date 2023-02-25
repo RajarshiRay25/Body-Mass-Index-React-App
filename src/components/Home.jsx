2fbe3bc38830960800235459b843dabc0624cc06
Rajarshi Ray <rayrajarshi9@gmail.com>
2023-02-25 23:07:55 +0530

    home page info

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,16 +7,16 @@ export default function Home() {
                 <h1 className="display-6 fw-bold">Welcome To Our Fitness & Health Monitoring App</h1>
                 <div className="col-lg-6 mx-auto">
                     <p className='lead mb-4 text-justify'>
-                        Welcome to our innovative online platform for tracking your body mass index (BMI) and nutritional intake! Our website provides you with the tools and resources to easily and accurately monitor your health and wellness. Whether you're looking to lose weight, gain muscle, or just maintain a healthy lifestyle, our website can help you achieve your goals.
-
-                        Our BMI calculator is easy to use and provides instant results based on your height and weight. You can set a goal weight and track your progress over time, seeing how your BMI changes as you work towards your target. Our site also includes nutritional tracking features, allowing you to log your daily meals and monitor your intake of key macronutrients like protein, carbohydrates, and fat.
-                        We believe that everyone should have access to the tools they need to take control of their health and wellness, and our server is designed to do just that. Whether you're a fitness enthusiast or just looking to make healthier choices in your daily life, our BMI and nutrition monitoring app is the perfect solution to help you achieve your goals.
+                        Welcome to our innovative online platform for tracking your body mass index (BMI) and nutritional intake! Our website provides you with the tools and resources to easily and accurately monitor your health and wellness. Whether you're looking to lose weight, gain muscle, or just maintain a healthy lifestyle, our site can help you achieve your goals. With our user-friendly BMI calculator and nutritional tracking features, you can track your progress and make informed decisions about your health. Join our community of fitness enthusiasts and health-conscious individuals today and start your journey towards a healthier you!
+                    </p>
+                    <p className='lead mb-4 text-justify'>
+                        Moreover we also have an Exercise Platform, where we provide you with personalized and effective exercise plans to help you achieve your fitness goals. Our platform allows you to easily search for exercises by muscle group, equipment, difficulty level, and more. Our website provides you with the best exercises for each muscle group, ensuring that you get the most out of your workout. Whether you are a beginner or an experienced fitness enthusiast, our platform has something for everyone. So, what are you waiting for? Let's get started on your fitness journey today!
                     </p>
                     <Link to="/bmi">
-                        <button className="btn btn-primary rounded my-4">Get Your BMI Count</button>
+                        <button className="btn btn-primary rounded my-0.5">Get Your BMI Count</button>
                     </Link>
                     <Link to="/nutcal">
-                        <button className="btn btn-success rounded my-4 mx-2">Get Your Nutrient Count</button>
+                        <button className="btn btn-success rounded my-0.5 mx-2">Get Your Nutrient Count</button>
                     </Link>
                 </div>
             </div>
