52d808755e5f591fdf5e33b3ecaa79bbf5548b24
Rajarshi Ray <rayrajarshi9@gmail.com>
2023-02-26 14:21:40 +0530

    About Page updated + Socials icons changed as per black and white theme.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import mypic from '../assets/myself.jpeg'
 import gitlogo from '../assets/git.png'
-import insta from '../assets/insta.png'
-import lin from '../assets/lin.png'
+import insta from '../assets/igbw.png'
+import lin from '../assets/lnked.png'
 import '../css/Aboutus.css'
 export default function About() {
     return (
@@ -10,16 +10,16 @@ export default function About() {
             <div className="row featurette">
                 <div className="col-md-7 order-md-2">
                     <h1 className="featurette-heading  lh-1">About Myself </h1>
-                    <p className="lead">Hello everyone , my name is Rajarshi Ray. I am a 3rd year student currently pursuing my B-Tech in Biotechnology in University of Engineering & Kolkata.I have made this website as a project which acts a simple UI for calculating BMI and nutritional contents in a user's desired food along with an exercise guidance support which can be highly beneficial for maintaining proper health and well being.</p>
+                    <p className="lead">Hello everyone , my name is Rajarshi Ray. I am a 3rd year student currently pursuing my B-Tech in Biotechnology in University of Engineering & Kolkata.I have made this website as a project which acts a simple UI for calculating BMI and nutritional contents in a user's desired food along with an exercise guidance support which will give you a wide range of exercises based on you desired muscle type which can be highly beneficial for maintaining proper health and well being.</p>
                     <h1 className="featurette-heading  lh-1">Contact Me </h1>
                     <span className="img1">
-                        <a  href="https://github.com/RajarshiRay25">
-                            <img  src={gitlogo} width="40px" height="40px" alt="" />
+                        <a href="https://github.com/RajarshiRay25">
+                            <img src={gitlogo} width="40px" height="40px" alt="" />
                         </a>
-                        <a  href="https://www.instagram.com/skylord_raj90/">
-                        <img src={insta} className="mx-4 my-3" width="40px" height="40px" alt="" />
+                        <a href="https://www.instagram.com/skylord_raj90/">
+                            <img src={insta} className="mx-4 my-3" width="40px" height="40px" alt="" />
                         </a>
-                        <a  href="https://linkedin.com/in/rajarshi-ray-5678721b7">
+                        <a href="https://linkedin.com/in/rajarshi-ray-5678721b7">
                             <img src={lin} width="40px" height="40px" alt="" />
                         </a>
 
